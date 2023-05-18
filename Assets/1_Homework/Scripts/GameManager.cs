07013db7cafd756c775f890dc2057128f55c4659
rPbl3yH <44815308+rPbl3yH@users.noreply.github.com>
2023-05-18 10:23:14 +0500

    feat: add obstacles

--- Assets/1_Homework/Scripts/GameManager.cs
+++ Assets/1_Homework/Scripts/GameManager.cs
@@ -14,15 +14,16 @@ public class GameManager : MonoBehaviour, IStartCounterFinishListener
     public GameState GameState => _gameState;
 
     [SerializeField] private GameUI _gameUI;
+
     private GameState _gameState;
     private List<IGameListener> _listeners = new();
     private List<IGameUpdateListener> _updateListeners = new();
     private List<IGameLateUpdateListener> _lateUpdatesListeners = new();
     private StartCounter _startCounter;
+    private ObstacleHitObservable _obstacleHitObservable;
 
     private void Awake() {
         var listeners = GetComponentsInChildren<IGameListener>();
-        print(listeners.Length);
 
         foreach (IGameListener gameListener in listeners) {
             AddListener(gameListener);
@@ -73,42 +74,70 @@ public class GameManager : MonoBehaviour, IStartCounterFinishListener
 
     [ContextMenu("Start game")]
     private void StartGame() {
+        if(_gameState == GameState.Playing || _gameState == GameState.Paused) {
+            return;
+        }
+
+        _obstacleHitObservable = new ObstacleHitObservable(this);
+        var obstacles = FindObjectsOfType<Obstacle>();
+
+        foreach (var obstacle in obstacles) {
+            obstacle.Init(_obstacleHitObservable);
+        }
+
         foreach (var gameListener in _listeners) {
             if (gameListener is IGameStartListener gameStartListener) {
                 gameStartListener.OnGameStarted();
             }
         }
 
-        _gameState = GameState.Playing;
+        SetState(GameState.Playing);
     }
 
-    [ContextMenu("Finish game")]
-    private void FinishGame() {
+    public void FinishGame() {
+        if(_gameState == GameState.Finished) {
+            return;
+        }
+
         foreach (var gameListener in _listeners) {
             if (gameListener is IGameFinishListener gameFinishListener) {
                 gameFinishListener.OnGameFinished();
             }
         }
-        _gameState = GameState.Finished;
+
+        SetState(GameState.Finished);
     }
 
     public void PauseGame() {
+        if(_gameState == GameState.Paused) {
+            return;
+        }
+
         foreach (var gameListener in _listeners) {
             if(gameListener is IGamePauseListener gamePauseListener) {
                 gamePauseListener.OnGamePaused();
             }
         }
 
-        _gameState = GameState.Paused;
+        SetState(GameState.Paused);
     }
 
     public void ResumeGame() {
+        if(_gameState == GameState.Playing) {
+            return;
+        }
+
         foreach(var gameListener in _listeners) {
             if(gameListener is IGameResumeListener gameResumeListener) {
                 gameResumeListener.OnGameResumed();
             }
         }
 
-        _gameState = GameState.Playing;
+        SetState(GameState.Playing);
+    }
+
+    private void SetState(GameState gameState) {
+        _gameState = gameState;
+        print($"Current game state = {gameState}");
     }
 }
---
+++ Assets/1_Homework/Scripts/Obstacle.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour, IGameStartListener, IGamePauseListener, IGameResumeListener, IGameFinishListener
+{
+    private IHitObservable _hitObservable;
+
+    void IGameStartListener.OnGameStarted() {
+        enabled = true;
+    }
+
+    void IGamePauseListener.OnGamePaused() {
+        enabled = false;
+    }
+
+    void IGameResumeListener.OnGameResumed() {
+        enabled = true;
+    }
+
+    void IGameFinishListener.OnGameFinished() {
+        enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.attachedRigidbody) {
+            if (other.attachedRigidbody.GetComponent<PlayerMoveController>()) {
+                _hitObservable.OnHit();
+            }
+        }
+    }
+
+    public void Init(IHitObservable hitObservable) {
+        _hitObservable = hitObservable;
+    }
+}
