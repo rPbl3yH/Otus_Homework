41566aa558796a498a13be592eb85e5853c504e0
rPbl3yH <44815308+rPbl3yH@users.noreply.github.com>
2023-05-27 14:07:25 +0500

    feat: character move controller refactoring

--- Assets/1_Homework/Scripts/CharacterMoveController.cs
+++ Assets/1_Homework/Scripts/CharacterMoveController.cs
@@ -1,8 +1,8 @@
 ﻿using UnityEngine;
 
-public class CharacterMoveController : MonoBehaviour, IGameStartListener, IGameFinishListener, IGameUpdateListener
+public class CharacterMoveController : MonoBehaviour, IGameStartListener, IGameFinishListener
 {
-    [SerializeField] private MoveComponent _moveController;
+    [SerializeField] private MoveComponent _moveComponent;
     [SerializeField] private MoveInput _moveInput;
     private RoadSide _roadSide;
 
@@ -13,10 +13,6 @@ public class CharacterMoveController : MonoBehaviour, IGameStartListener, IGameF
 
     void IGameFinishListener.OnGameFinished() {
         _moveInput.OnMove -= OnMoveHorizontal;
-    }
-
-    void IGameUpdateListener.OnUpdate(float deltaTime) {
-        _moveController.MoveForward(deltaTime);
     }
 
     private void OnMoveHorizontal(InputDirection inputDirection) {
@@ -31,7 +27,7 @@ public class CharacterMoveController : MonoBehaviour, IGameStartListener, IGameF
         UpdateRoadSide(inputDirection);
 
         var dx = inputDirection == InputDirection.Left ? Vector3.left.x : Vector3.right.x;
-        _moveController.MoveHorizonral(dx);
+        _moveComponent.MoveHorizonral(dx);
     }
 
     private void UpdateRoadSide(InputDirection inputDirection) {
---
+++ Assets/1_Homework/Scripts/MoveForwardController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class MoveForwardController : MonoBehaviour, IGameUpdateListener
+{
+    [SerializeField] private MoveComponent _moveComponent;
+
+    void IGameUpdateListener.OnUpdate(float deltaTime) {
+        _moveComponent.MoveForward(deltaTime);
+    }
+}
