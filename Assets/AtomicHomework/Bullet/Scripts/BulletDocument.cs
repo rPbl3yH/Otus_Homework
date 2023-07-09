1298dfb861d42725654796aef7f99b8e2ee226ff
rPbl3yH <44815308+rPbl3yH@users.noreply.github.com>
2023-07-10 01:47:06 +0500

    feat: add bullet

---
+++ Assets/AtomicHomework/Bullet/Scripts/BulletDocument.cs
@@ -0,0 +1,24 @@
+﻿using AtomicHomework.Atomic.Custom;
+using Declarative;
+using Lessons.Gameplay.Atomic1;
+using UnityEngine;
+
+namespace AtomicHomework.Bullet.Scripts
+{
+    public class BulletDocument : DeclarativeModel
+    {
+        public Transform Transform;
+        public AtomicVariable<float> Speed;
+
+        public FixedUpdateMechanics FixedUpdateMechanics = new();
+        
+        [Construct]
+        public void Construct()
+        {
+            FixedUpdateMechanics.OnUpdate(deltaTime =>
+            {
+                Transform.Translate(Vector3.forward * (Speed.Value * deltaTime));
+            });
+        }
+    }
+}
--- Assets/AtomicHomework/Hero/Fire.cs
+++ Assets/AtomicHomework/Hero/Fire.cs
@@ -8,6 +8,8 @@ namespace AtomicHomework.Hero
     [Serializable]
     public class Fire
     {
+        public GameObject BulletPrefab;
+        public Transform SpawnPoint;
         public AtomicVariable<int> Delay;
 
         public AtomicEvent OnFire = new();
@@ -17,8 +19,7 @@ namespace AtomicHomework.Hero
         {
             OnFire += () =>
             {
-                Debug.Log("Fire");
-                //TODO: fire
+                GameObject.Instantiate(BulletPrefab, SpawnPoint.position, SpawnPoint.rotation);
             };
         }
     }
