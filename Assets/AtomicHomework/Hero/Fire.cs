31d049134c5292b85c74d965d377e77960784cf9
rPbl3yH <44815308+rPbl3yH@users.noreply.github.com>
2023-07-10 08:24:28 +0500

    feat: add cooldown for fire

--- Assets/AtomicHomework/Hero/Fire.cs
+++ Assets/AtomicHomework/Hero/Fire.cs
@@ -18,6 +18,9 @@ namespace AtomicHomework.Hero
 
         public AtomicVariable<bool> IsCanAttack;
         public TimerMechanics TimerMechanics;
+
+        [Section]
+        public FireCooldown FireCooldown;
         
         
         [Construct]
@@ -47,7 +50,52 @@ namespace AtomicHomework.Hero
 
             BulletCount.OnChanged += count =>
             {
-                IsCanAttack.Value = count > 0;
+                if (count > 0)
+                {
+                    IsCanAttack.Value = !FireCooldown.IsCooldown.Value;
+                }
+                else
+                {
+                    IsCanAttack.Value = false;
+                }
+            };
+
+            FireCooldown.IsCooldown.OnChanged += isCooldown =>
+            {
+                if (!isCooldown)
+                {
+                    IsCanAttack.Value = BulletCount.Value > 0;
+                }
+            };
+        }
+    }
+    
+    [Serializable]
+    public class FireCooldown
+    {
+        public AtomicVariable<float> CoolDown;
+        public TimerMechanics TimerMechanics;
+
+        public AtomicVariable<bool> IsCooldown;
+
+        [Construct]
+        public void Construct(Fire fire)
+        {
+            TimerMechanics.Construct(CoolDown.Value);
+                
+            fire.OnFire += () =>
+            {
+                if (!IsCooldown.Value)  
+                {
+                    IsCooldown.Value = true;
+                    TimerMechanics.StartTimer();
+                }
+            };
+
+            TimerMechanics.OnTimerFinished += () =>
+            {
+                IsCooldown.Value = false;
+                TimerMechanics.StopTimer();
             };
         }
     }
